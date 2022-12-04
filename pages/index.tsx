b81b09881d74c23ed858066264aa72c64ad00ccb
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-04 21:01:25 +0330

    Theme done

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,13 @@ import { ThemeProvider } from "@mui/material/styles";
 import Theme from "../Theme/Theme";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 export default function Home() {
   return (
     <ThemeProvider theme={Theme}>
       <Grid>
         <CssBaseLine />
-        <Grid height="30px" bgcolor="secondary.main">
-          <Typography variant="h2">Hello World</Typography>
-          <Button variant="outlined">افزودن به سبد</Button>
-          <Button variant="contained">نهایی کردن خرید</Button>
-          <Grid color="primary.main"> dffsdafsdafdsa</Grid>
-        </Grid>
       </Grid>
     </ThemeProvider>
   );
