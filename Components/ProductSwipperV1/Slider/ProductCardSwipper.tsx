e2e2bf454ff5d7e9cd91de61a0be60329479ec4d
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-05 00:21:15 +0330

    create component swipper v1

---
+++ Components/ProductSwipperV1/Slider/ProductCardSwipper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ProductCardSwipper = (props: Props) => {
+  return (
+    <div>ProductCardSwipper</div>
+  )
+}
+
+export default ProductCardSwipper
