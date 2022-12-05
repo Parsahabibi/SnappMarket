ea5ca6c41eb53a56f52d8a8ad8a8948866137d99
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-05 18:03:49 +0330

    profileButtonHeader created.

---
+++ Components/ProfileButtonHeader/ProfileButtonHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import { Grid } from "@mui/material";
+
+type Props = {};
+
+const ProfileButtonHeader = (props: Props) => {
+  return (
+    <Grid>
+      <Grid></Grid>
+    </Grid>
+  );
+};
+
+export default ProfileButtonHeader;
