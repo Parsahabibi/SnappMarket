999b2cfd2f6e0818ee4b395edcbea76270fa68ad
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-05 10:05:50 +0330

    create components and data file

---
+++ Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+type Props = {}
+
+const CardProductSwiper = (props: Props) => {
+  return (
+    <Grid>CardProductSwiper</Grid>
+  )
+}
+
+export default CardProductSwiper
---
+++ Components/ProductSwiper Version1/Slider/Slider.tsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+import CardProductSwiper from '../CardProductSwiper/CardProductSwiper'
+
+type Props = {}
+
+const Slider = (props: Props) => {
+  return (
+    <Grid>
+        Slider
+        <CardProductSwiper/>
+    </Grid>
+  )
+}
+
+export default Slider
