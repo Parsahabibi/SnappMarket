42b75ebb7bd5a2c19508c81eb4146c86d53334b6
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-05 18:20:49 +0330

    ProfileButtonHeader added.

--- Components/Header/DekstopHeader.tsx
+++ Components/Header/DekstopHeader.tsx
@@ -3,6 +3,7 @@ import Grid from "@mui/material/Grid";
 import React from "react";
 import Container from "@mui/system/Container";
 import ShoppingBasketButton from "../ShoppingBasketButton/ShoppingBasketButton";
+import ProfileButtonHeader from "../ProfileButtonHeader/ProfileButtonHeader";
 
 type Props = {};
 
@@ -10,7 +11,12 @@ const DekstopHeader = (props: Props) => {
   return (
     <Grid padding="20px" bgcolor="success.main" height="14rem">
       <Container maxWidth="lg">
-        <ShoppingBasketButton />
+        <Grid display="flex">
+          <Grid display="flex" justifyContent="space-evenly">
+            <ShoppingBasketButton />
+            <ProfileButtonHeader />
+          </Grid>
+        </Grid>
       </Container>
     </Grid>
   );
--- Components/ProfileButtonHeader/ProfileButtonHeader.tsx
+++ Components/ProfileButtonHeader/ProfileButtonHeader.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import ButtonBase from "@mui/material/ButtonBase";
 
 type Props = {};
 
 const ProfileButtonHeader = (props: Props) => {
   return (
-    <Grid>
-      <Grid></Grid>
-    </Grid>
+    <ButtonBase
+      sx={{
+        ml: "20px",
+        alignself: "center",
+        background: "transparent",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Typography>ahmad </Typography>
+      <Person2OutlinedIcon sx={{ ml: "5px" }} />
+    </ButtonBase>
   );
 };
 
