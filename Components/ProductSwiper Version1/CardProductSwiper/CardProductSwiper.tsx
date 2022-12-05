09f9f0ef2d9196f472046e26f3c1ce19b1a7b948
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-05 17:43:28 +0330

    add slider v1 done

--- Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
+++ Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
@@ -3,28 +3,29 @@ import Image from 'next/image'
 import React from 'react'
 import { dataCard } from '../../../Data/DataCardProductSwiperV1/DataCardProductSwiperV1'
 import ButtonProductCardSwiper from '../../ButtonProductCardSwiper/ButtonProductCardSwiper'
+import { dataCardSliderType } from "../../../Model/DataCardSliderV1Type";
 
 
-const CardProductSwiper = () => {
+const CardProductSwiper = ({title,image,Price,Weight,priceReduction,Discount}:dataCardSliderType) => {
   return (
-    <Grid display='flex' gap={2} flexWrap={'wrap'}>
-      {dataCard.map(card =>
+    <Grid display='flex' gap={2} width={'400px'} >
+      {/* {datamap(card => */}
         <Grid bgcolor={'common.white'} borderRadius={'0.5rem'} width={234} height={'100%'} px={2}>
           <Grid display='flex' justifyContent={'center'} my={1}>
-            <Image src={card.image} alt={card.title} width={170} height={170} />
+            <Image src={image} alt={title} width={170} height={170} />
           </Grid>
-          <Grid height={60}> <Typography variant='h3' fontSize={'1rem'}>{card.title}</Typography></Grid>
-          <Typography variant='subtitle1' fontSize={'0.8rem'}>{card.Weight}</Typography>
+          <Grid height={60}> <Typography variant='h3' fontSize={'1rem'}>{title}</Typography></Grid>
+          <Typography variant='subtitle1' fontSize={'0.8rem'}>{Weight}</Typography>
           <Grid display='flex' alignItems={'center'} gap={1} py={1}>
-            <Typography sx={{ bgcolor: 'secondary.main', color: 'common.white', borderRadius: 1, p: '2px 4px' }}>{card.Discount}</Typography>
-            <Typography sx={{ textDecoration: 'line-through', fontSize: '12px', fontWeight: '500' }} color={'info.main'}>{card.priceReduction}</Typography>
+            <Typography sx={{ bgcolor: 'secondary.main', color: 'common.white', borderRadius: 1, p: '2px 4px' }}>{Discount}</Typography>
+            <Typography sx={{ textDecoration: 'line-through', fontSize: '12px', fontWeight: '500' }} color={'info.main'}>{priceReduction}</Typography>
           </Grid>
           <Grid display='flex' justifyContent={'space-between'} >
-            <Typography pt={1.2} sx={{ fontSize: '12px' }} pb={5}>{card.Price}تومان</Typography>
+            <Typography pt={1.2} sx={{ fontSize: '12px' }} color={'common.black'} pb={5}>{Price}تومان</Typography>
             <ButtonProductCardSwiper />
           </Grid>
         </Grid>
-      )}
+      {/* )} */}
     </Grid>
   )
 }
