f53e54336c8008588407e31e3215776ee790eec3
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-05 11:47:07 +0330

    create data card done

---
+++ Data/DataCardProductSwiperV1/DataCardProductSwiperV1.ts
@@ -0,0 +1,74 @@
+import image1 from "../../assets/Images/image1.jpg";
+import image2 from "../../assets/Images/image2.jpg";
+import image3 from "../../assets/Images/image3.jpg";
+import image4 from "../../assets/Images/image4.jpg";
+import image5 from "../../assets/Images/image5.jpg";
+import image6 from "../../assets/Images/image6.jpg";
+import image7 from "../../assets/Images/image7.jpg";
+import { dataCardSliderType } from "../../Model/DataCardSliderV1Type";
+
+export const dataCard: dataCardSliderType[] = [
+  {
+    id: 1,
+    image: image1,
+    title: "شامپو بدنRelaxبوتانیس ",
+    Weight: "400 میلی لیتر",
+    priceReduction: "75,000",
+    Discount: "%30",
+    Price: "52,500",
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "کره حیوانی شکلی",
+    Weight: "50 گرم",
+    priceReduction: "13,500",
+    Discount: "%10",
+    Price: "12,150",
+  },
+  {
+    id: 3,
+    image: image3,
+    title: "شکر سفید الماس دانه",
+    Weight: "900 گرم",
+    priceReduction: "23,230",
+    Discount: "%14",
+    Price: "19,978",
+  },
+  {
+    id: 4,
+    image: image4,
+    title: "زعفران مصطفوی 1مثقالی",
+    Weight: "100 گرم",
+    priceReduction: "263,700",
+    Discount: "%35",
+    Price: "171,405",
+  },
+  {
+    id: 5,
+    image: image5,
+    title: "لازانیا پیش پخت زر ماکارون",
+    Weight: "300 گرم",
+    priceReduction: "31,000",
+    Discount: "%15",
+    Price: "26.350",
+  },
+  {
+    id: 6,
+    image: image6,
+    title: "پودر ماشین لباسشویی لاوندر پرسیل",
+    Weight: "500 گرم",
+    priceReduction: "19,687",
+    Discount: "%13",
+    Price: "17,128",
+  },
+  {
+    id: 7,
+    image: image7,
+    title: "کنسرو ماهی تون در روغن محفل",
+    Weight: "150 گرم",
+    priceReduction: "37,400",
+    Discount: "%22",
+    Price: "29,172",
+  },
+];
---
+++ Model/DataCardSliderV1Type.ts
@@ -0,0 +1,9 @@
+export interface dataCardSliderType {
+    id: number,
+    title:string,
+    image:any | string,
+    Weight:number | string,
+    Discount:number | string,
+    Price:string,
+    priceReduction:number | string
+}
