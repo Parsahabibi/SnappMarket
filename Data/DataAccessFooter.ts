0a14aa14e5be9b5e99adf169faa4c6a0a1e556f8
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-06 18:13:35 +0330

    add dataAccessFooter and typing data done

---
+++ Data/DataAccessFooter.ts
@@ -0,0 +1,10 @@
+import { dataAccessFooterType } from "../Model/DataAccessFooterType";
+
+export const dataAccessFooter:dataAccessFooterType[]=[
+    {id:1,title:'درباره‌ما',href:'#'},
+    {id:2,title:'وبلاگ',href:'#'},
+    {id:3,title:'قوانین',href:'#'},
+    {id:4,title:'استخدام',href:'#'},
+    {id:5,title:'گزارش مشکل',href:'#'},
+    {id:6,title:'تبلیغات در اسنپ مارکت',href:'#'},
+]
