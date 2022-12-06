beae7e97bbc3d8add889953e49a3add8986f580f
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-06 18:12:46 +0330

    add type and data logo footer done

---
+++ Data/DataLogoFooter.ts
@@ -0,0 +1,9 @@
+import { dataLogoFooterType } from "../Model/DataLogoFooterType";
+import logo1 from '../assets/Images/ImageFooter/logo.png'
+import logo2 from '../assets/Images/ImageFooter/logo (1).png'
+import logo3 from '../assets/Images/ImageFooter/logo(2).png'
+export const dataLogoFooter:dataLogoFooterType[]=[
+    {id:1,image:logo1},
+    {id:2,image:logo2},
+    {id:3,image:logo3},
+]
