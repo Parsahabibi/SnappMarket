4382245bfbef72889b3de28317cd5927d9481fbd
Aboufazel <a.ab2521@gmail.com>
2022-12-07 19:06:57 +0330

    swiper add in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import Grid from "@mui/material/Grid";
 import CssBaseLine from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "../Theme/Theme";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 import SecondarySwiperContainer from "../components/SecondarySwiper/SecondarySwiperContainer/SecondarySwiperContainer";
 
 export default function Home() {
