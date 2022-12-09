45ff9a2d1088fbb00a969875370852de4618fa81
Aboufazel <a.ab2521@gmail.com>
2022-12-09 19:56:11 +0330

    add two component in project

---
+++ Components/SecondarySwiper/SecondarySwiperCard/SecondarySwiperCard.tsx
@@ -0,0 +1,88 @@
+import {Box} from "@mui/system";
+import Grid from "@mui/material/Grid";
+import {Button, Typography} from "@mui/material";
+import Image from "next/image";
+import sample from "../../../assets/Images/image6.jpg"
+
+const SecondarySwiperCard = () => {
+
+    return(
+        <Grid
+              m={"12px"}
+              display={"flex"}
+              sx={{backgroundColor:"common.white" , width:"330px" , height:"220px" , borderRadius:"5px" ,flexDirection:"column"}}>
+           <Grid display={"flex"} p={"12px"} width={"100%"} justifyContent={"space-between"}>
+               <Typography sx={{fontSize:"14px" , fontWeight:500}}>
+                   {"پک آماده مصرف"}
+               </Typography>
+               <Typography sx={{fontSize:"12px" , fontWeight:400 , cursor:"pointer" , color:"primary.main"}}>
+                   {"مشاهده همه"}
+               </Typography>
+           </Grid>
+            <Grid marginY={"3px"} display={"flex"} position={"relative"} width={"100%"}>
+                <Box mr={"12px"}
+                     display={"flex"}
+                     border={"1px solid rgb(202, 211, 254)"}
+                     width={"60px"} height={"60px"}
+                     alignItems={"center"}
+                     bgcolor={"common.white"}
+                     justifyContent={"center"}
+                     sx={{borderRadius:"50%"}}>
+                    <Image src={sample} alt={"product"} width={55} style={{borderRadius:"50%"}}/>
+                </Box>
+                <Box position={"absolute"}
+                     right={"55px"}
+                     display={"flex"}
+                     border={"1px solid rgb(202, 211, 254)"}
+                     width={"60px"} height={"60px"}
+                     alignItems={"center"}
+                     justifyContent={"center"}
+                     bgcolor={"common.white"}
+                     sx={{borderRadius:"50%"}}>
+                    <Image src={sample} alt={"product"} width={55} style={{borderRadius:"50%"}}/>
+                </Box>
+            </Grid>
+            <Grid marginY={"5px"}
+                  marginX={"12px"}
+                  display={"flex"}
+                  alignItems={"center"}
+                  width={"100%"}>
+                <Typography ml={1} sx={{color:"secondary.main" , fontSize:"12px"}}>
+                    {"20% تخفیف بیشتر"}
+                </Typography>
+                <Typography sx={{fontSize:"12px"}}>
+                    {"با خرید این بسته"}
+                </Typography>
+            </Grid>
+            <Grid display={"flex"}
+                  mr={"12px"} mt={1}>
+                <Box bgcolor={"secondary.main"}
+                     p={0.1}
+                     ml={1}
+                     sx={{color:"common.white" , fontSize:"14px" , borderRadius:"3px"}}>
+                    {"27%"}
+                </Box>
+                <del style={{fontSize:"14px"}}>
+                    {"22,000"}
+                </del>
+            </Grid>
+            <Grid display={"flex"}
+                  justifyContent={"space-between"}
+                  width={"90%"}
+                  marginX={"12px"}
+                  mt={1}
+                  alignItems={"center"}>
+                <Typography sx={{fontSize:"16px"}}>
+                    {"16,016 تومان"}
+                </Typography>
+                <Button variant={"outlined"}
+                        sx={{height:"32px" , fontSize:"12px" , width:"max-content" , padding:"2px"}}>
+                    {"افزودن"}
+                </Button>
+            </Grid>
+        </Grid>
+    )
+}
+
+
+export default SecondarySwiperCard;
---
+++ Components/SecondarySwiper/SecondarySwiperCard/SeeAllCard.tsx
@@ -0,0 +1,24 @@
+import Grid from "@mui/material/Grid";
+import {Typography} from "@mui/material";
+
+
+const SeeAllCard = () => {
+
+    return(
+        <Grid display={"flex"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              m={"12px"}
+              width={"150px"}
+              bgcolor={"common.white"}
+              sx={{borderRadius:"5px" , cursor:"pointer"}}
+              height={"220px"}>
+              <Typography sx={{color:"primary.main"}}>
+                  {"مشاهده همه"}
+              </Typography>
+        </Grid>
+    )
+}
+
+
+export default SeeAllCard;
