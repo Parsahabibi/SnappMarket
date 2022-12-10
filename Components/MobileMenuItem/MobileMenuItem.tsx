615483efe8fa9d3c2dd586fcf0e019bc8b95b099
Parsa <97088319+Parsahabibi@users.noreply.github.com>
2022-12-10 21:43:11 +0330

    feat: MobileMenuItem Done

--- Components/MobileMenuItem/MobileMenuItem.tsx
+++ Components/MobileMenuItem/MobileMenuItem.tsx
@@ -2,37 +2,37 @@ import React from 'react'
 import { Grid, Typography } from '@mui/material'
 import Link from 'next/link'
 
-import HomeIcon from '@mui/icons-material/Home';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import QrCodeIcon from '@mui/icons-material/QrCode';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import QrCodeOutlinedIcon from '@mui/icons-material/QrCodeOutlined';
 
 type Props = {}
 
 const MobileMenuItem = (props: Props) => {
     return (
         <Grid>
-            <Grid bgcolor={'common.white'} container justifyContent={"space-around"} position={'fixed'} bottom={0} left={0} right={0} borderTop={1} borderColor="rgb(255, 255, 255)">
+            <Grid bgcolor={'common.white'} container justifyContent={"space-around"} position={'fixed'} bottom={0} left={0} right={0}>
                 <Grid pt={1}>
                     <Link href={""}>
-                        <Grid container flexDirection={"column"} alignItems={"center"} justifyContent={"center"} color={"rgb(26, 28, 35)"}>
-                            <HomeIcon />
-                            <Typography fontSize={'12px'}>خانه</Typography>
+                        <Grid container flexDirection={"column"} alignItems={"center"} justifyContent={"center"} color={"primary.main"}>
+                            <HomeOutlinedIcon style={{width:"25px" , height:"25px"}}/>
+                            <Typography color={"primary.main"} variant='subtitle2'>خانه</Typography>
                         </Grid>
                     </Link>
                 </Grid>
                 <Grid pt={1}>
                     <Link href={""}>
                         <Grid container flexDirection={"column"} alignItems={"center"} justifyContent={"center"} color={"rgb(26, 28, 35)"}>
-                            <QrCodeIcon />
-                            <Typography fontSize={'12px'}>دسته بندی ها</Typography>
+                            <QrCodeOutlinedIcon style={{width:"25px" , height:"25px"}} />
+                            <Typography variant='subtitle2'>دسته بندی ها</Typography>
                         </Grid>
                     </Link>
                 </Grid>
                 <Grid pt={1}>
                     <Link href={""}>
                         <Grid container flexDirection={"column"} alignItems={"center"} justifyContent={"center"} color={"rgb(26, 28, 35)"}>
-                            <ShoppingCartIcon />
-                            <Typography fontSize={'12px'}>سبد خرید</Typography>
+                            <ShoppingCartOutlinedIcon style={{width:"25px" , height:"25px"}} />
+                            <Typography variant='subtitle2'>سبد خرید</Typography>
                         </Grid>
                     </Link>
                 </Grid>
