b9ebd98254ce712e8501d17cd0c36c4c304f79d3
Aboufazel <a.ab2521@gmail.com>
2022-12-12 19:57:29 +0330

    merge : merge mobile selector Branch to the main

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Grid from "@mui/material/Grid";
 import CssBaseLine from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "../Theme/Theme";
-import SecondarySwiperContainer from "../components/SecondarySwiper/SecondarySwiperContainer/SecondarySwiperContainer";
 import MobileStoreSelectore from "../Components/MobileStoreSelectore/MobileStoreSelectore";
 
 
