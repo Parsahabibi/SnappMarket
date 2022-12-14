92d61606afaa9fb604493f323e82209d31fe4a06
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-14 18:41:06 +0330

    add data card and refactor data card swiper done

---
+++ Data/DataMobileCategoryCard/DataMobileCategoryCard.ts
@@ -0,0 +1,172 @@
+import image1 from "../../assets/Images/ImageCardMobileCategory/image1.jpg";
+import image2 from "../../assets/Images/ImageCardMobileCategory/image2.jpg";
+import image3 from "../../assets/Images/ImageCardMobileCategory/image3.jpg";
+import image4 from "../../assets/Images/ImageCardMobileCategory/image4.jpg";
+import image5 from "../../assets/Images/ImageCardMobileCategory/image5.jpg";
+import image6 from "../../assets/Images/ImageCardMobileCategory/image6.jpg";
+import image7 from "../../assets/Images/ImageCardMobileCategory/image7.jpg";
+import image8 from "../../assets/Images/ImageCardMobileCategory/image8.jpg";
+import image9 from "../../assets/Images/ImageCardMobileCategory/image9.jpg";
+import image10 from "../../assets/Images/ImageCardMobileCategory/image10.jpg";
+import image11 from "../../assets/Images/ImageCardMobileCategory/image11.jpg";
+import image12 from "../../assets/Images/ImageCardMobileCategory/image12.jpg";
+import image13 from "../../assets/Images/ImageCardMobileCategory/image13.jpg";
+import image14 from "../../assets/Images/ImageCardMobileCategory/image14.jpg";
+import image15 from "../../assets/Images/ImageCardMobileCategory/image15.jpg";
+import { dataCardSliderType } from "../../Model/DataCardSliderV1Type";
+export const dataMobileCardCategory: dataCardSliderType[] = [
+  {
+    id: 1,
+    image: image1,
+    title: "رژگونه شماره ۰۳ سیترای",
+    Weight: "400 میلی لیتر",
+    priceReduction: "75,000",
+    Discount: "%30",
+    Price: "52,500",
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "کرم پودر Ultra Wear شماره ۳۰۸ ماردینی ۳۰ میلی لیتری",
+    Weight: "50 گرم",
+    priceReduction: "13,500",
+    Discount: "%10",
+    Price: "12,150",
+  },
+  {
+    id: 3,
+    image: image3,
+    title: "کرم پودر Ultra Wear شماره ۳۰۷ ماردینی ۳۰ میلی لیتری",
+    Weight: "900 گرم",
+    priceReduction: "23,230",
+    Discount: "%14",
+    Price: "19,978",
+  },
+  {
+    id: 4,
+    image: image4,
+    title: "پنکیک Rose Peach شماره ۲۰ این لی",
+    Weight: "100 گرم",
+    priceReduction: "263,700",
+    Discount: "%35",
+    Price: "171,405",
+  },
+  {
+    id: 5,
+    image: image5,
+    title: "ریمل حجم دهنده Wonderful Lashes شماره ۲۲ سیترای",
+    Weight: "300 گرم",
+    priceReduction: "31,000",
+    Discount: "%15",
+    Price: "26.350",
+  },
+  {
+    id: 6,
+    image: image6,
+    title: "کانسیلر دور چشم Corpex شماره ۱۰۲ پریم ۱۵ میلی لیتری",
+    Weight: "500 گرم",
+    priceReduction: "19,687",
+    Discount: "%13",
+    Price: "17,128",
+  },
+  {
+    id: 7,
+    image: image7,
+    title: "بالم لب Aloe Vera کامان ۳.۵ گرمی",
+    Weight: "150 گرم",
+    priceReduction: "37,400",
+    Discount: "%22",
+    Price: "29,172",
+  },
+ 
+  {
+    id:8,
+    image: image8,
+    title: 'بالم لب Aloe Vera کامان ۳.۵ گرمی',
+    Weight: "150 گرم",
+    priceReduction: "37,400",
+    Discount: "%22",
+    Price: "29,172",
+  },
+  {
+    id: 9,
+    image: image9,
+    title: 'رژ لب جامد Matte Effect شماره ۲۱۸ ماردینی',
+    Weight: "150 گرم",
+    priceReduction: "37,400",
+    Discount: "%22",
+    Price: "29,172",
+  },
+  {
+    id: 10,
+    image: image10,
+    title: 'کرم پودر Matt Makeup سری Black Diamond شماره ۰۳ مای ۳۵ میلی لیتری',
+    Weight: "400 میلی لیتر",
+    priceReduction: "75,000",
+    Discount: "%30",
+    Price: "52,500",
+  },
+  {
+    id: 11,
+    image: image11,
+    title: 'ریمل ابرو شماره ۰۱ کنویس',
+    Weight: "50 گرم",
+    priceReduction: "13,500",
+    Discount: "%10",
+    Price: "12,150",
+  },
+  {
+    id: 12,
+    image: image12,
+    title: 'کرم پودر شماره ۲۱ روبی سان ۳۰ میلی لیتری',
+    Weight: "900 گرم",
+    priceReduction: "23,230",
+    Discount: "%14",
+    Price: "19,978",
+  },
+  {
+    id: 11,
+    image: image11,
+    title: 'رژگونه شماره ۲۵۳ روبی سان',
+    Weight: "100 گرم",
+    priceReduction: "263,700",
+    Discount: "%35",
+    Price: "171,405",
+  },
+  {
+    id: 12,
+    image: image12,
+    title: 'کرم پودر Matt Makeup سری Black Diamond شماره ۰۱ مای ۳۵ میلی لیتری',
+    Weight: "300 گرم",
+    priceReduction: "31,000",
+    Discount: "%15",
+    Price: "26.350",
+  },
+  {
+    id: 13,
+    image: image13,
+    title: 'لاک ناخن شماره ۱۱۶ وایت کیوت',
+    Weight: "500 گرم",
+    priceReduction: "19,687",
+    Discount: "%13",
+    Price: "17,128",
+  },
+  {
+    id: 14,
+    image: image14,
+    title: "لاک ناخن شماره ۸۰۶ مای",
+    Weight: "150 گرم",
+    priceReduction: "37,400",
+    Discount: "%22",
+    Price: "29,172",
+  },
+  {
+    id: 15,
+    image: image15,
+    title: 'رژ لب جامد شماره ۱۳۰ روبی سان',
+    Weight: "150 گرم",
+    priceReduction: "37,400",
+    Discount: "%22",
+    Price: "29,172",
+  },
+];
--- Model/DataCardSliderV1Type.ts
+++ Model/DataCardSliderV1Type.ts
@@ -1,9 +1,15 @@
 export interface dataCardSliderType {
-    id: number,
-    title:string,
-    image:any | string,
-    Weight:number | string,
-    Discount:number | string,
-    Price:string,
-    priceReduction:number | string
+  id: number;
+  title: string;
+  image: any | string;
+  Weight: number | string;
+  Discount: number | string;
+  Price: string;
+  priceReduction: number | string;
+  width: string;
+  titleBtn: string | any;
+  widthImage: number;
+  heightImage: number;
+  bottom: string;
+  left: string;
 }
