c08c78d3bed6e4a898123f496d4bc527c0e4d999
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-22 13:23:44 +0330

    create data sidebar done

---
+++ Data/DataSidebarCategory/DataSidebarCategory.ts
@@ -0,0 +1,52 @@
+import { dataFilterSidebarType } from "../../Model/DataSidebarCategoryType";
+
+export const dataFilterSidebar: dataFilterSidebarType[] = [
+  {
+    id: 1,
+    title: "دسته بندی",
+    subset: [
+      { id: 2, checkbox: "شیر تازه" },
+      { id: 3, checkbox: "شیر مدت دار" },
+    ],
+  },
+  {
+    id: 4,
+    title: "برند",
+    subset: [
+      { id: 5, checkbox: "پاگ" },
+      { id: 6, checkbox: "پگاه " },
+      { id: 7, checkbox: "دامداران" },
+      { id: 8, checkbox: "دنت" },
+      { id: 9, checkbox: "دومینو" },
+      { id: 10, checkbox: "رامک" },
+      { id: 11, checkbox: "روزانه" },
+      { id: 12, checkbox: "شیرین عسل" },
+      { id: 13, checkbox: "صباح" },
+      { id: 14, checkbox: "عالیس" },
+      { id: 15, checkbox: "کاله" },
+      { id: 16, checkbox: "مانداسوی" },
+      { id: 17, checkbox: "مانیزان" },
+      { id: 18, checkbox: "میهن" },
+      { id: 19, checkbox: "نیچرلین" },
+      { id: 20, checkbox: "هراز" },
+    
+    ],
+  },
+  {
+    id: 21,
+    title: "نوع",
+    subset: [
+      { id: 22, checkbox: "ساده" },
+      { id: 23, checkbox: "توت فرنگی" },
+      { id: 24, checkbox: " پر چرب" },
+      { id: 25, checkbox: "کم چرب" },
+      { id: 26, checkbox: "طالبی" },
+      { id: 27, checkbox: "موز" },
+      { id: 28, checkbox: "نیم چرب" },
+      { id: 29, checkbox: "کاکائو" },
+      { id: 30, checkbox: "شیرسویا" },
+      { id: 31, checkbox: "قهوه" },
+     
+    ],
+  },
+];
---
+++ Model/DataSidebarCategoryType.ts
@@ -0,0 +1,10 @@
+export interface dataFilterSidebarType {
+  id: number;
+  title: string;
+  subset: [
+    {
+      checkbox: string;
+      id:number
+    }
+  ];
+}
