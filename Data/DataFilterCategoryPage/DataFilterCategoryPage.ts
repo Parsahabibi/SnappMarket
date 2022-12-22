8c203110e0ddc463950c174d09d04df1690cfe04
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-22 13:23:06 +0330

    create data filter done

---
+++ Data/DataFilterCategoryPage/DataFilterCategoryPage.ts
@@ -0,0 +1,12 @@
+import { dataFilterCategoryPageType } from "../../Model/DataFilterCategoryPageType";
+
+
+export const dataFilterCategoryPage:dataFilterCategoryPageType[]=[
+    {id:1,title:'پیشفرض'},
+    {id:2,title:'پر فروش ترین'},
+    {id:3,title:'بیشترین درصد تخفیف'},
+    {id:4,title:'بیشترین مبلغ تخفیف'},
+    {id:5,title:'ارزان ترین'},
+    {id:6,title:'گران ترین'},
+ 
+]
