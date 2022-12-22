9e1e97b5404426095fd2db23d49fb484061fef19
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-22 13:22:41 +0330

    refactor card productSiwper done

--- Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
+++ Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
@@ -1,13 +1,41 @@
 import { Button, Grid, Typography } from '@mui/material'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import ButtonProductCardSwiper from '../../ButtonProductCardSwiper/ButtonProductCardSwiper'
 import { dataCardSliderType } from "../../../Model/DataCardSliderV1Type";
+type Props = {
+  title: string;
+  image: any | string;
+  Weight: number | string;
+  Discount: number | string;
+  Price: string;
+  priceReduction: number | string;
+  width: string;
+  titleBtn: string | any;
+  widthImage: number;
+  heightImage: number;
+  bottom: string;
+  left: string;
+  hoverStyle:{};
+  normalStyle:{}
+}
 
 
-const CardProductSwiper = ({ bottom, left, title, image, Price, Weight, priceReduction, Discount, width, titleBtn, heightImage, widthImage }: dataCardSliderType) => {
+const CardProductSwiper = ({ bottom, left, title, image, Price, Weight, priceReduction, Discount, width, titleBtn, heightImage, widthImage,normalStyle,hoverStyle }: Props) => {
+  const [hover, setHover] = useState(false)
+ 
+  const onMouseEnter = () => {
+    setHover(true)
+  }
+
+  const onMouseLeave = () => {
+    setHover(false)
+  }
+
+
+
   return (
-    <Grid display='flex' gap={2} width={width} sx={{ cursor: "pointer" }}  >
+    <Grid display='flex' gap={2} width={width} sx={{ cursor: "pointer" }} style={hover ? hoverStyle : normalStyle} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <Grid position={'relative'} bgcolor={'common.white'} borderRadius={'0.5rem'} width={234} px={2} >
         <Grid display='flex' justifyContent={'center'} my={1}>
           <Image src={image} alt={title} width={widthImage} height={heightImage} />
@@ -21,7 +49,7 @@ const CardProductSwiper = ({ bottom, left, title, image, Price, Weight, priceRed
         <Grid display='flex' justifyContent={'space-between'} >
           <Typography pt={1.2} sx={{ fontSize: '12px' }} color={'common.black'} pb={2}>{Price}تومان</Typography>
           <Grid position={'absolute'} sx={{ bottom: bottom, left: left }}>
-            <Grid display={{xs:'none',sm:'flex'}}> <ButtonProductCardSwiper title={titleBtn}  bgcolor={''} value={''} icon={''}/></Grid>
+            <Grid display={{ xs: 'none', sm: 'flex' }}> <ButtonProductCardSwiper title={titleBtn} bgcolor={''} value={''} icon={''} /></Grid>
             <Button sx={{ fontWeight: 400, cursor: 'pointer', display: { xs: 'flex', sm: 'none' } }}><Typography border={'0.1rem solid rgba(35, 71, 251, 0.24)'} display={'flex'} justifyContent={'center'} alignItems={'center'} width={'3rem'} height={'3rem'} boxShadow={1} borderRadius={'50%'}>{titleBtn}</Typography></Button>
           </Grid>
         </Grid>
