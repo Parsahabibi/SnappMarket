31ed202e6828012d725a91b7bbbc909eae7cf3da
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-24 09:48:20 +0330

    extra files deleted

--- Components/BasketModal/BasketLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-export const BasketLayout = (props: Props) => {
-  return <div></div>;
-};
--- Components/Header/DekstopHeader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-export const DekstopHeader = (props: Props) => {
-  return <div></div>;
-};
