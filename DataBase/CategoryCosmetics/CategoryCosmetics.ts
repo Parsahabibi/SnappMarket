f93851afff3c8fe2f0ae833e356441df4b10029b
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-26 16:43:08 +0330

    create file database cosmetics done

---
+++ DataBase/CategoryCosmetics/CategoryCosmetics.ts
@@ -0,0 +1,24 @@
+export const DataBaseCosmetics = [
+  {
+    id: 2,
+    name: "",
+    categories: ["محصولات آرایشی", "بهداشت و مراقبت صورت","شامپو و مراقبت مو","آرایش مو","بهداشت و مراقبت بدن","بهداشت دهان","اصلاح صورت وبدن"],
+    cosmetics: [
+        {
+            id: 1,
+            category: "",
+            image: "",
+            title: "",
+            Weight: "",
+            priceReduction: "",
+            Discount: "",
+            Price: "",
+            Brand: "",
+            Seller:"",
+            ProductFeatures:[
+                {}
+            ]
+          },
+    ],
+  },
+];
