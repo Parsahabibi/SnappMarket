fae7be52ad5879f6d91ee58f60f8bb8cfd4c51fb
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-26 15:47:01 +0330

    AboutUse fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import CssBaseLine from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "../Theme/Theme";
 import MobileStoreSelectore from "../Components/MobileStoreSelectore/MobileStoreSelectore";
-
+import AboutUs from "../Components/AboutUs/AboutUs";
 
 export default function Home() {
   return (
     <ThemeProvider theme={Theme}>
       <Grid>
         <CssBaseLine />
-          <MobileStoreSelectore/>
+        <MobileStoreSelectore />
+        <AboutUs />
       </Grid>
     </ThemeProvider>
   );
