b1d7e968e31bf580b8e28a135124af78c48cf30a
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-12-26 15:43:15 +0330

    style : mobile store selectore Style fixed

--- Components/ButtonProductCardSwiper/ButtonProductCardSwiper.tsx
+++ Components/ButtonProductCardSwiper/ButtonProductCardSwiper.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
-import Button from '@mui/material/Button'
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 type Props = {
-  title: string
-  icon: any,
-  bgcolor: string,
-  value:string
-}
+  title: string;
+  icon: any;
+  bgcolor: string;
+  value: string;
+};
 
-const ButtonProductCardSwiper = ({ title, icon, bgcolor,value }: Props) => {
+const ButtonProductCardSwiper = ({ title, icon, bgcolor, value }: Props) => {
   return (
     <Grid>
-      <Button variant='outlined' sx={{ color: bgcolor, cursor: 'pointer' }}>{title} {icon} {value}</Button>
+      <Button
+        variant="outlined"
+        sx={{
+          color: bgcolor,
+          cursor: "pointer",
+          fontSize: "1.2rem",
+          fontWeight: "bold",
+          lineHeight: "normal",
+          fontStretch: "normal",
+          fontStyle: "normal",
+          letterSpacing: "normal",
+        }}
+      >
+        {title} {icon} {value}
+      </Button>
     </Grid>
-  )
-}
+  );
+};
 
-export default ButtonProductCardSwiper
+export default ButtonProductCardSwiper;
--- Components/MobileStoreSelectore/MobileStoreSelectoreCard/MobileStoreSelectoreCard.tsx
+++ Components/MobileStoreSelectore/MobileStoreSelectoreCard/MobileStoreSelectoreCard.tsx
@@ -1,42 +1,75 @@
-import React from 'react'
-import { Grid, Typography } from '@mui/material'
-import Image from 'next/image'
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import Image from "next/image";
 type Props = {
-  title: string,
-  image: string | any,
-  Discount: string,
-  dis: string,
-  bgcolor: string,
-  bgcolor2: string,
-  backgroundimage: string | any
-}
+  title: string;
+  image: string | any;
+  Discount: string;
+  dis: string;
+  bgcolor: string;
+  bgcolor2: string;
+  backgroundimage: string | any;
+};
 
-const MobileStoreSelectoreCard = ({ title, image, dis, Discount, bgcolor, bgcolor2, backgroundimage }: Props) => {
+const MobileStoreSelectoreCard = ({
+  title,
+  image,
+  dis,
+  Discount,
+  bgcolor,
+  bgcolor2,
+  backgroundimage,
+}: Props) => {
   const styling = {
     backgroundImage: `url('${backgroundimage.src}')`,
     width: "100%",
-    backgroundSize: '70px',
-    backgroundRepeat: 'no-repeat',
-
-  }
+    backgroundSize: "70px",
+    backgroundRepeat: "no-repeat",
+  };
 
   return (
-    <Grid style={styling} display={'flex'}   px={0.8} justifyContent={'space-between'} borderRadius={'1rem'} alignItems={'center'} mb={13} bgcolor={bgcolor} height={103}>
-      <Grid display={'flex'} gap={1.3} >
+    <Grid
+      style={styling}
+      display={"flex"}
+      px={0.8}
+      justifyContent={"space-between"}
+      borderRadius={"1rem"}
+      alignItems={"center"}
+      mb={13}
+      bgcolor={bgcolor}
+      height={103}
+    >
+      <Grid display={"flex"} gap={1.3}>
         <Grid mt={1}>
-          <Image src={image} alt={title} width={40} height={40} style={{ borderRadius: '50%' }} />
+          <Image
+            src={image}
+            alt={title}
+            width={40}
+            height={40}
+            style={{ borderRadius: "50%" }}
+          />
         </Grid>
-        <Grid color={'common.white'} >
-          <Typography fontSize={'1.7rem'} fontWeight={'bold'} mb={'0.8rem'}>{title}</Typography>
-          <Typography fontSize={'1.4rem'} fontWeight={'normal'} mb={'0.8rem'}>{dis}</Typography>
+        <Grid color={"common.white"}>
+          <Typography fontSize={"1.7rem"} fontWeight={"bold"} mb={"0.8rem"}>
+            {title}
+          </Typography>
+          <Typography fontSize={"1.4rem"} fontWeight={"normal"} mb={"0.8rem"}>
+            {dis}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid borderRadius={'0.4rem'} bgcolor={bgcolor2} mb={'3rem'}>
-        <Typography fontSize={'1.3rem'} fontWeight={500} p={'0px 0.8rem'} color={'common.white'}>{Discount}</Typography>
+      <Grid borderRadius={"0.4rem"} bgcolor={bgcolor2} mb={"3rem"}>
+        <Typography
+          fontSize={"1.3rem"}
+          fontWeight={500}
+          p={"0px 0.8rem"}
+          color={"common.white"}
+        >
+          {Discount}
+        </Typography>
       </Grid>
     </Grid>
+  );
+};
 
-  )
-}
-
-export default MobileStoreSelectoreCard
+export default MobileStoreSelectoreCard;
