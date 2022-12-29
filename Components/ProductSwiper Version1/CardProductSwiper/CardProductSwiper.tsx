f8ec03c06818f4f606816b2c41f821bc530459d1
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-29 12:45:08 +0330

    refactor title button card done

--- Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
+++ Components/ProductSwiper Version1/CardProductSwiper/CardProductSwiper.tsx
@@ -21,7 +21,7 @@ const CardProductSwiper = ({ bottom, left, title, image, Price, Weight, priceRed
     </Grid>
     <Grid display='flex' justifyContent={'space-between'} >
       <Typography pt={1.2} sx={{ fontSize: '1.6rem' }} color={'common.black'} pb={2}>{Price}تومان</Typography>
-      <ButtonProductCardSwiper title={''} icon={undefined} bgcolor={''} value={''} />
+      <ButtonProductCardSwiper title={'افزودن به سبد'} icon={undefined} bgcolor={''} value={''} />
     </Grid>
   
 </Grid>
