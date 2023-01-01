5c44e590b0a24975949593fab044d2fad7e17ab6
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-01-01 15:21:18 +0330

    refactor auctionBanner done

--- Components/Banner/AuctionBanner.tsx
+++ Components/Banner/AuctionBanner.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 const AuctionBanner = (props: Props) => {
     return (
-        <Grid display={{sm:'flex'}}>
+        <Grid display={{sm:'flex'}} mt={{xs:26,sm:0}}>
             <Container>
                 <Grid mt={{xs:2,sm:5}} mb={{xs:2,sm:4}} height={{xs:'60px',sm:'110px'}}>
                     <Image src={image1} alt={'logo'} style={{ width: '100%', height: '100%', borderRadius: '0.5rem' }} />
