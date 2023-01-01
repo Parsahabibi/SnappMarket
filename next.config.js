d54e640ae7b8abd88d6e9e36bed28a0df51013dd
Aboufazel <a.ab2521@gmail.com>
2023-01-02 00:08:31 +0330

    Ref : next config refactor

--- next.config.js
+++ next.config.js
@@ -6,7 +6,14 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['cdn.snapp.market'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '',
+      },
+    ],
   },
 }
 
