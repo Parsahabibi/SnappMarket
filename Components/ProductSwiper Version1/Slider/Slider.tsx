b8a21fa2366986abdbdebb821bd1015285764d11
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-01-01 15:27:17 +0330

    refactor swiper slider done

--- Components/ProductSwiper Version1/Slider/Slider.tsx
+++ Components/ProductSwiper Version1/Slider/Slider.tsx
@@ -25,8 +25,7 @@ type Props = {
   color: string
 }
 
-const Slider = ({ color, title }: Props) => {
-
+const Slider = ({ color, title}: Props) => {
   const styling = {
     backgroundImage: `url('${backgroundImage.src}')`,
     width: "100%",
@@ -47,10 +46,15 @@ const Slider = ({ color, title }: Props) => {
   useEffect(() => {
     swiperRef.current.swiper.slideTo(page)
   }, [page])
+  const hoverStyle = {
 
+  }
+
+  const normalStyle = {
+  }
   return (
     <Container>
-      <Grid display={{xs:'none',sm:'flex'}} flexDirection={'column'} style={styling} bgcolor={color} color={'common.white'} position={'relative'}>
+      <Grid display={{ xs: 'none', sm: 'flex' }} flexDirection={'column'} style={styling} bgcolor={color} color={'common.white'} position={'relative'}>
         {page > 0 ?
           <Grid boxShadow={5} position={'absolute'} top={'45%'} right={15} display={'flex'} zIndex={99} alignItems={'center'} justifyContent={'center'} width={40} height={40} bgcolor={'primary.main'} borderRadius={'50%'} color={'common.white'}>
             <Button sx={{ color: 'common.white' }} onClick={handlePrevSlide}><ArrowForwardIosIcon /></Button>
@@ -78,7 +82,7 @@ const Slider = ({ color, title }: Props) => {
             ref={swiperRef}
             virtual
           >
-            <SwiperSlide style={{maxWidth:'30px'}}>
+            <SwiperSlide style={{ maxWidth: '30px' }}>
               <Grid height={'100%'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
                 <Link href={'#'}>
                   <Typography variant='h2' color={'common.white'}>{title}</Typography>
@@ -87,16 +91,16 @@ const Slider = ({ color, title }: Props) => {
             </SwiperSlide>
             {dataCard.map(slide => (
               <SwiperSlide key={slide.id}>
-                <Grid my={1} display={'flex'} alignItems={'flex-start'}> <CardProductSwiper title={slide.title} image={slide.image} Price={slide.Price} Weight={slide.Weight} priceReduction={slide.priceReduction} Discount={slide.Discount} id={0} width={'23.4rem'} titleBtn={'افزودن به سبد'} widthImage={170} heightImage={170} bottom={'85%'} left={'5%'} /></Grid>
+                <Grid my={1} display={'flex'} alignItems={'flex-start'}> <CardProductSwiper title={slide.title} image={slide.image} Price={slide.Price} Weight={slide.Weight} priceReduction={slide.priceReduction} Discount={slide.Discount} id={0} width={'23.4rem'} titleBtn={'افزودن به سبد'} widthImage={170} heightImage={170} bottom={'15%'} left={'50%'} hoverStyle={hoverStyle} normalStyle={normalStyle} /></Grid>
               </SwiperSlide>
             ))}
             <SwiperSlide >
-              <Typography  width={'95%'} bgcolor={'common.white'} mt={1} height={'32rem'} display={'flex'} sx={{ borderRadius: '5px',cursor:'pointer' }} justifyContent={'center'} alignItems={'center'} color={'secondary.main'} >{'مشاهده بیشتر'}</Typography>
+              <Typography width={'95%'} bgcolor={'common.white'} mt={1} height={'32rem'} display={'flex'} sx={{ borderRadius: '5px', cursor: 'pointer' }} justifyContent={'center'} alignItems={'center'} color={'secondary.main'} >{'مشاهده بیشتر'}</Typography>
             </SwiperSlide>
           </Swiper>
           {page < 2 ?
             <Grid position={'absolute'} top={'45%'} left={15} zIndex={99} display={'flex'} alignItems={'center'} justifyContent={'center'} width={40} height={40} bgcolor={'primary.main'} borderRadius={'50%'} boxShadow={5} >
-              <Button sx={{ color: 'common.white',pl:'13px' }} onClick={handleNextSlide}><ArrowBackIosIcon /></Button>
+              <Button sx={{ color: 'common.white', pl: '13px' }} onClick={handleNextSlide}><ArrowBackIosIcon /></Button>
             </Grid>
             : ''}
         </Grid>
