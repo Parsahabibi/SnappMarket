bcb6beb6814a3489b53f12f784b351d5eb2cbc91
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-01-03 23:23:14 +0330

    add logik shopping basket done

--- Components/ShoppingBasketButton/ShoppingBasketButton.tsx
+++ Components/ShoppingBasketButton/ShoppingBasketButton.tsx
@@ -4,12 +4,17 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import ButtonBase from "@mui/material/ButtonBase";
 import { Box } from "@mui/system";
+import { useSelector } from "react-redux";
+import { cartSlice } from "../../redux/Slice/Slice";
+import store from "../../redux/store";
 
 type Props = {
   handleModalOpen: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const ShoppingBasketButton = ({ handleModalOpen }: Props) => {
+  const cartItems = useSelector((state:any) => state.cart);
+  const totalCount = cartItems.reduce((prev:any, p:any) => prev + p.count, 0);
   return (
     <Grid>
       <ButtonBase
@@ -42,7 +47,7 @@ const ShoppingBasketButton = ({ handleModalOpen }: Props) => {
           }}
           component="span"
         >
-          1
+          {totalCount}
         </Box>
       </ButtonBase>
     </Grid>
