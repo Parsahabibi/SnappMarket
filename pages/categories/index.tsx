f9cfe814589ce394811f381a342014b0d25af244
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-01-03 21:43:10 +0330

    create route categoris done

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,13 +1,20 @@
 import { Grid } from '@mui/material'
 import React from 'react'
-import Category from '../../Components/CategotyMobileMenu/Category'
+import CategoryPage from '../../Components/CategoryPage/CategoryPage'
+import DekstopHeader from '../../Components/Header/DekstopHeader'
+import MobileCategoryPage from '../../Components/MobileCategoryPage/MobileCategoryPage'
+import Header from '../../Components/MobileMenuHeader/Header'
+
 
 type Props = {}
 
 const categories = (props: Props) => {
   return (
     <Grid>
-        <Category position={undefined} display={''} bgcolor={undefined} pb={''} ptop={''}/>
+      <DekstopHeader />
+      {/* <Header /> */}
+      <CategoryPage />
+      <MobileCategoryPage/>
     </Grid>
   )
 }
